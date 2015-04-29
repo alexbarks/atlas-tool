89d90d16b385117f0bf9bcc4bc0355cb7cd795f0
alex.l8arker <alex.l8arker@785685b2-fbad-5b69-4ac7-40b3334fc1ca>
2015-04-29 22:37:27 +0000

    update to instructions

--- view_WSS_vectors.m
+++ view_WSS_vectors.m
@@ -1,6 +1,6 @@
 clc, clear, close all
 
-load L:\cv_mri\Aorta-4D_Flow\Results\Pim\Data\MIMICS\BAV_tissue\BAV\23_PT315-SC\mrstruct\mask_struct_aorta
+load d:\research\MR_GT_WSSvalidation\phantom_tube\pulsatile\70BPM_3LMIN\mrstruct_subject_20150324_user009\mask_struct_aorta.mat
 mask2 = mrstruct_mask.dataAy;
 mask2_vox = mrstruct_mask.vox;
 
@@ -11,7 +11,7 @@ contours(L2==1) = 1;
 [F,V] = isosurface(contours,0); % make a surface from the detected contours
 V = V .* (ones(size(V,1),1) * mask2_vox(1:3));
 
-load L:\cv_mri\Aorta-4D_Flow\Results\Pim\Data\MIMICS\BAV_tissue\BAV\23_PT315-SC\mrstruct\Wss_point_cloud_aorta
+load d:\research\MR_GT_WSSvalidation\phantom_tube\pulsatile\70BPM_3LMIN\mrstruct_subject_20150324_user009\Wss_point_cloud_aorta
 WSS_all = Wss_point_cloud; clear Wss_point_cloud
 WSS = WSS_all{3};
 
